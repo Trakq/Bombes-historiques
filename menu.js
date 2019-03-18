5621ad2f7d2878a46017bd507b9c206388c7dd72
Jakob <jakoboul@hotmail.com>
2019-03-18 12:13:58 -0400

    Prototype ajout de marqueur

--- menu.js
+++ menu.js
@@ -5,7 +5,8 @@ var app = express();
 //Portion de code provenant de https://developers.google.com/maps/documentation/javascript/examples/map-simple
 app.get('/menu', function(req, res) {
     res.setHeader('Content-Type', 'text/html');
-    res.send('<!DOCTYPE html>\n' +
+    res.send(
+        '<!DOCTYPE html>\n' +
         '<html>\n' +
         '  <head>\n' +
         '    <title>Simple Map</title>\n' +
@@ -40,7 +41,10 @@ app.get('/menu', function(req, res) {
         '    async defer></script>\n' +
         '  </body>\n' +
         '</html>');
+
 })
+
+
     
     .get('/mainPage', function(req, res) {
         res.setHeader('Content-Type', 'text/html');
@@ -59,24 +63,67 @@ app.listen(8080);
 //---------------------------------------------------------------------------------------------------------------------------------------
 //Mettre un marqueur lors du click
 //-------------------------------------------------------------------------------------------------------------------------------------------------
-/*function initMap() {
+/*
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Marker Labels</title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map.
+#map {
+    height: 100%;
+}
+/* Optional: Makes the sample page fill the window.
+html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+</style>
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
+    <script>
+// In the following example, markers appear when the user clicks on the map.
+// Each marker is labeled with a single alphabetical character.
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+
+function initialize() {
+    var bangalore = { lat: 12.97, lng: 77.59 };
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: {lat: -25.363882, lng: 131.044922 }
+        zoom: 12,
+        center: bangalore
     });
 
-    map.addListener('click', function(e) {
-        placeMarkerAndPanTo(e.latLng, map);
+
+
+    // This event listener calls addMarker() when the map is clicked.
+    google.maps.event.addListener(map, 'click', function(event) {
+        addMarker(event.latLng, map);
     });
+
+    // Add a marker at the center of the map.
+    addMarker(bangalore, map);
 }
 
-function placeMarkerAndPanTo(latLng, map) {
+// Adds a marker to the map.
+function addMarker(location, map) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
     var marker = new google.maps.Marker({
-        position: latLng,
+        position: location,
+        label: labels[labelIndex++ % labels.length],
         map: map
     });
-    map.panTo(latLng);
 }
-//------------------------------------------------------------------------------------------------------------------------------------------------------
-})*/
 
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+</head>
+<body>
+<div id="map"></div>
+    </body>
+    </html>*/
