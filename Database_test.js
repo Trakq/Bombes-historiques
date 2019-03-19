e2e30b7f8a889b2a30ae843efd7ed208f2776ad5
Laurent Arsenault <laurentarsenault8@gmail.com>
2019-03-19 10:46:12 -0400

    Avancement connection db et fonctions pour infos

--- Database_test.js
+++ Database_test.js
@@ -1,5 +1,31 @@
-/*let myQuery = 'select * from patate where num = ?';
-let params = [inputValue] ;
-// run query (myQuery, params)*/
 
 
+const { Client } = require('pg');
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'bombes_historiques',
+    password: 'Bananes19',
+    port: 5432,
+});
+
+async function bienvenue() {
+    await client.connect();
+    const res = await client.query('SELECT $1::text as message', ['Hello world!']);
+    console.log(res.rows[0].message); // Hello world!
+    await client.end()
+}
+
+async function bob() {
+    try {
+        await bienvenue()
+    } catch (e) {
+        console.error(e.message)
+    }
+}
+
+bob();
+
+/*getInfoBombes(3, function() {
+    console.log.....
+})*/
