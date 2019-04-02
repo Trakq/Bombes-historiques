69fbc71b8f7decb2779858b4775ca8cc681485a7
wilfried <wilfried2202@hotmail.com>
2019-04-02 11:18:26 -0400

    correction des migrations

--- migrations/20190329133149-add-people.js
+++ migrations/20190329133149-add-people.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20190329133149-add-people-up.sql');
+  var filePath = path.join(__dirname, 'sqls', 'bombes_historiques_db.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -33,6 +33,7 @@ exports.up = function(db) {
   });
 };
 
+/*
 exports.down = function(db) {
   var filePath = path.join(__dirname, 'sqls', '20190329133149-add-people-down.sql');
   return new Promise( function( resolve, reject ) {
@@ -46,7 +47,7 @@ exports.down = function(db) {
   .then(function(data) {
     return db.runSql(data);
   });
-};
+};*/
 
 exports._meta = {
   "version": 1
